c08f46a7dc0348be7d79866b990f9695da0ad049
Zach Olivier <olivierzach618@gmail.com>
2026-05-31 20:47:45 -0700

    Type () to ensure children render function is defined

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import '../app.css';
   // Svelte 5 runes: layout receives a `children` render function.
-  let { children } = $props();
+  let { children } = $props<{ children: any }>();
 </script>
 
 <svelte:head>
